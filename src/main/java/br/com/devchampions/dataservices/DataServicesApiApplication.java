a6ea35670647c0c13a1cd6ebd85a451612332e27
Dherkyan R. Silva <dherkyan@gmail.com>
2025-07-27 03:24:44 -0300

    Iníio  do rojeto Data Services API

---
+++ src/main/java/br/com/devchampions/dataservices/DataServicesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.devchampions.dataservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataServicesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataServicesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devchampions/dataservices/DataServicesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devchampions.dataservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataServicesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
