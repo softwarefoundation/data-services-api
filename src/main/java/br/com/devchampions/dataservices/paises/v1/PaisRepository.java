c2ac0222ae3c6b1b7d05de8426f1ff7f224a52d3
Dherkyan R. Silva <dherkyan@gmail.com>
2025-08-03 05:25:19 -0300

    Remoção de método não utilizados

--- src/main/java/br/com/devchampions/dataservices/paises/v1/PaisRepository.java
+++ src/main/java/br/com/devchampions/dataservices/paises/v1/PaisRepository.java
@@ -5,15 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface PaisRepository extends JpaRepository<Pais, Long>, JpaSpecificationExecutor<Pais> {
-
-    Optional<Pais> findByAlpha2(final String alpha2);
-
-    Optional<Pais> findByAlpha3(final String alpha3);
-
-    Optional<Pais> findByDominio(final String dominio);
 
 }
--- src/main/java/br/com/devchampions/dataservices/paises/v1/PaisService.java
+++ src/main/java/br/com/devchampions/dataservices/paises/v1/PaisService.java
@@ -1,14 +1,11 @@
 package br.com.devchampions.dataservices.paises.v1;
 
 
-import br.com.devchampions.dataservices.exceptions.RegistroNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @AllArgsConstructor
 @Service
@@ -24,33 +21,6 @@ public class PaisService {
     @Transactional(readOnly = true)
     public Page<Pais> buscarPais(PaisSpecification paisSpecification, Pageable pageable) {
         return paisRepository.findAll(paisSpecification, pageable);
-    }
-
-    @Transactional(readOnly = true)
-    public Pais paisPorCodigoAlpha2(final String alpha2) {
-        if (alpha2 == null || alpha2.trim().isEmpty()) {
-            throw new IllegalArgumentException("Código Alpha2 não pode ser nula ou vazia");
-        }
-
-        return paisRepository.findByAlpha2(alpha2).orElseThrow(() -> new RegistroNotFoundException("Registro não localizado pelo código: ".concat(alpha2)));
-    }
-
-    @Transactional(readOnly = true)
-    public Pais paisPorCodigoAlpha3(final String alpha3) {
-        if (alpha3 == null || alpha3.trim().isEmpty()) {
-            throw new IllegalArgumentException("Código alpha3 não pode ser nula ou vazia");
-        }
-
-        return paisRepository.findByAlpha3(alpha3).orElseThrow(() -> new RegistroNotFoundException("Registro não localizado pelo código: ".concat(alpha3)));
-    }
-
-    @Transactional(readOnly = true)
-    public Pais paisPorCodigoDominio(final String dominio) {
-        if (dominio == null || dominio.trim().isEmpty()) {
-            throw new IllegalArgumentException("Código Domínio não pode ser nula ou vazia");
-        }
-
-        return paisRepository.findByDominio(dominio).orElseThrow(() -> new RegistroNotFoundException("Registro não localizado pelo código: ".concat(dominio)));
     }
 
 }
